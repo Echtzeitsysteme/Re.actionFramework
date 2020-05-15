e6397aec38aa93cd32f517997cbe2ac64dc0291e
tobnie <niehues.tobias@gmail.com>
2020-05-15 11:15:15 +0200

    fixed build error for empty .react-files

--- org.reaction.ibex/src/org/reaction/build/simsg/ReactionModelBuilder.java
+++ org.reaction.ibex/src/org/reaction/build/simsg/ReactionModelBuilder.java
@@ -56,7 +56,7 @@ import SimulationDefinition.SimulationDefinitionFactory;
 import SimulationDefinition.StochasticRate;
 
 public class ReactionModelBuilder implements ModelBuilderExtension {
-	
+
 	public static final Logger logger = Logger.getLogger(ReactionModelBuilder.class);
 
 	@Override
@@ -64,7 +64,7 @@ public class ReactionModelBuilder implements ModelBuilderExtension {
 		logger.log(Priority.INFO, "Running SimSGModelBuilder for Re.action:");
 		double tic = System.currentTimeMillis();
 		ReactionLanguageStandaloneSetup.doSetup();
-		
+
 		logger.log(Priority.INFO, "Creating metamodels from specifications..");
 		List<IFile> reactFiles = null;
 		try {
@@ -73,45 +73,52 @@ public class ReactionModelBuilder implements ModelBuilderExtension {
 			logger.error("Could not gather necessary .react-files. Error: \n" + e.getMessage());
 			return;
 		}
-		
+
 		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
 		ResourceSet resourceSet = new ResourceSetImpl();
 		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
 		Map<IFile, ReactionModel> editorModels = new HashMap<>();
-		
+
 		reactFiles.forEach(reactFile -> {
 			URI uri = URI.createPlatformResourceURI(reactFile.getFullPath().toString(), true);
 			Resource file = resourceSet.getResource(uri, true);
 			try {
 				EcoreUtil2.resolveLazyCrossReferences(file, () -> false);
 			} catch (WrappedException e) {
-				logger.log(Priority.INFO, String.format("Error resolving cross references in file %s.", reactFile.getName()));
+				logger.log(Priority.INFO,
+						String.format("Error resolving cross references in file %s.", reactFile.getName()));
 				return;
 			}
 
-			ReactionModel editorModel = (ReactionModel) file.getContents().get(0);
-			editorModels.put(reactFile, editorModel);
+			if (!file.getContents().isEmpty()) {
+				ReactionModel editorModel = (ReactionModel) file.getContents().get(0);
+				editorModels.put(reactFile, editorModel);
+			}
 		});
 		EcoreUtil.resolveAll(resourceSet);
-		
+
 		Map<ReactionModel, IntermediateModelContainer> intermediateModels = new HashMap<>();
 		editorModels.values().forEach(editorModel -> {
 			IntermTransformation trafo = new IntermTransformation(editorModel);
 			intermediateModels.put(editorModel, trafo.generateIntermediateModel());
 		});
-		
+
 		IFolder folder = project.getFolder(SimSGBuilder.DEFAULT_METAMODEL_LOCATION);
 		IFolder instancesFolder = project.getFolder(SimSGBuilder.DEFAULT_MODEL_LOCATION);
 		intermediateModels.values().forEach(iModel -> {
-			saveResource(iModel, folder.getFullPath().toPortableString()+"/"+iModel.getName()+"_intermediate.xmi");
+			saveResource(iModel,
+					folder.getFullPath().toPortableString() + "/" + iModel.getName() + "_intermediate.xmi");
 		});
-		
+
 		Map<ReactionModel, EPackage> metaModels = new HashMap<>();
 		Map<ReactionModel, ReactionContainer> containerModels = new HashMap<>();
 		intermediateModels.forEach((editorModel, intermediateModel) -> {
 			ContainerGenerator gen = new ContainerEMF(intermediateModel);
 			try {
-				gen.doGenerate(instancesFolder.getFullPath().toPortableString()+"/"+intermediateModel.getName()+"_container.xmi", folder.getFullPath().toPortableString()+"/"+intermediateModel.getName()+".ecore");
+				gen.doGenerate(
+						instancesFolder.getFullPath().toPortableString() + "/" + intermediateModel.getName()
+								+ "_container.xmi",
+						folder.getFullPath().toPortableString() + "/" + intermediateModel.getName() + ".ecore");
 				metaModels.put(editorModel, gen.getMetamodel());
 				org.eclipse.emf.ecore.EPackage.Registry reg = EPackage.Registry.INSTANCE;
 				reg.put(gen.getMetamodel().getNsURI(), gen.getMetamodel());
@@ -121,74 +128,78 @@ public class ReactionModelBuilder implements ModelBuilderExtension {
 				return;
 			}
 		});
-		
+
 		Map<ReactionModel, IBeXPatternSet> ibexPatterns = new HashMap<>();
 		intermediateModels.forEach((editorModel, intermediateModel) -> {
 			IBeXCreator creator = new IBeXCreator(intermediateModel, metaModels.get(editorModel));
 			ibexPatterns.put(editorModel, creator.getIBeXPatternSet());
-			creator.savePatternSet(folder.getFullPath().toPortableString()+"/"+intermediateModel.getName()+"_ibexpatterns.xmi");
+			creator.savePatternSet(
+					folder.getFullPath().toPortableString() + "/" + intermediateModel.getName() + "_ibexpatterns.xmi");
 		});
-		
+
 		Map<ReactionModel, GTRuleSet> gtRules = new HashMap<>();
 		intermediateModels.forEach((editorModel, intermediateModel) -> {
 			GTCreator creator = new GTCreator(ibexPatterns.get(editorModel));
 			gtRules.put(editorModel, creator.getGTRuleSet());
-			creator.saveRuleSet(folder.getFullPath().toPortableString()+"/"+intermediateModel.getName()+"_gtrules.xmi");
+			creator.saveRuleSet(
+					folder.getFullPath().toPortableString() + "/" + intermediateModel.getName() + "_gtrules.xmi");
 		});
-		
+
 		IFolder definitionsFolder = project.getFolder(SimSGBuilder.DEFAULT_DEFINITION_LOCATION);
 		Map<ReactionModel, SimDefinition> definitions = new HashMap<>();
 		metaModels.forEach((editorModel, metamodel) -> {
 			GTRuleSet gtRulesSet = gtRules.get(editorModel);
 			IBeXPatternSet ibexPatternSet = ibexPatterns.get(editorModel);
 			IntermediateModelContainer intermediateModel = intermediateModels.get(editorModel);
-			
+
 			SimulationDefinitionGenerator gen = new SimulationDefinitionGenerator(metamodel.getName());
 			gen.addMetaModel(metamodel);
 			gen.setGtRules(gtRulesSet);
 			gen.setIBeXPatterns(ibexPatternSet);
-			gen.setModelURI(instancesFolder.getLocation()+"/"+intermediateModel.getName()+"_container.xmi");
+			gen.setModelURI(instancesFolder.getLocation() + "/" + intermediateModel.getName() + "_container.xmi");
 //			gen.setModelURI(containerModels.get(editorModel).eResource().getURI().);
-			
+
 			addComponentsToDefinition(gen.getDefinition(), gtRulesSet, ibexPatternSet, intermediateModel);
-			gen.saveDefinition(definitionsFolder.getFullPath().toPortableString()+"/"+intermediateModel.getName()+"_definition.xmi");
+			gen.saveDefinition(definitionsFolder.getFullPath().toPortableString() + "/" + intermediateModel.getName()
+					+ "_definition.xmi");
 			definitions.put(editorModel, gen.getDefinition());
 		});
-		
-		//cleanup
+
+		// cleanup
 		intermediateModels.values().stream().filter(x -> x.eResource() != null).forEach(x -> x.eResource().unload());
 		metaModels.values().stream().filter(x -> x.eResource() != null).forEach(x -> x.eResource().unload());
 		ibexPatterns.values().stream().filter(x -> x.eResource() != null).forEach(x -> x.eResource().unload());
 		gtRules.values().stream().filter(x -> x.eResource() != null).forEach(x -> x.eResource().unload());
 		definitions.values().stream().filter(x -> x.eResource() != null).forEach(x -> x.eResource().unload());
 		containerModels.values().stream().filter(x -> x.eResource() != null).forEach(x -> x.eResource().unload());
-		
+
 		double toc = System.currentTimeMillis();
-		logger.log(Priority.INFO, "Creating metamodels from specifications.. Done! ("+ (toc-tic)/1000.0 + " seconds.)");
+		logger.log(Priority.INFO,
+				"Creating metamodels from specifications.. Done! (" + (toc - tic) / 1000.0 + " seconds.)");
 	}
-	
+
 	public static void saveResource(EObject model, String path) {
 		ResourceSet rs = new ResourceSetImpl();
 		rs.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
-		
+
 		URI uri = URI.createFileURI(path);
 		Resource modelResource = rs.createResource(uri);
 		modelResource.getContents().add(model);
-		
-		Map<Object, Object> saveOptions = ((XMIResource)modelResource).getDefaultSaveOptions();
-		saveOptions.put(XMIResource.OPTION_ENCODING,"UTF-8");
+
+		Map<Object, Object> saveOptions = ((XMIResource) modelResource).getDefaultSaveOptions();
+		saveOptions.put(XMIResource.OPTION_ENCODING, "UTF-8");
 		saveOptions.put(XMIResource.OPTION_USE_XMI_TYPE, Boolean.TRUE);
-		saveOptions.put(XMIResource.OPTION_SAVE_TYPE_INFORMATION,Boolean.TRUE);
+		saveOptions.put(XMIResource.OPTION_SAVE_TYPE_INFORMATION, Boolean.TRUE);
 		saveOptions.put(XMIResource.OPTION_SCHEMA_LOCATION_IMPLEMENTATION, Boolean.TRUE);
-		
+
 		try {
-			((XMIResource)modelResource).save(saveOptions);
+			((XMIResource) modelResource).save(saveOptions);
 		} catch (IOException e) {
-			LogUtils.error(logger, "Couldn't save resource: \n "+e.getMessage());
+			LogUtils.error(logger, "Couldn't save resource: \n " + e.getMessage());
 		}
 //		modelResource.unload();
 	}
-	
+
 	/**
 	 * Returns the list of .react files.
 	 * 
@@ -196,58 +207,57 @@ public class ReactionModelBuilder implements ModelBuilderExtension {
 	 */
 	private List<IFile> getFiles(final IProject project) throws Exception {
 		List<IFile> files = new LinkedList<>();
-		crawlSubfolders(project.getFolder(SimSGBuilder.DEFAULT_SRC_LOCATION), files); 
+		crawlSubfolders(project.getFolder(SimSGBuilder.DEFAULT_SRC_LOCATION), files);
 		return files.stream() //
 				.filter(f -> "react".equals(f.getFileExtension()) && f.exists()) //
 				.collect(Collectors.toList());
 	}
-	
+
 	private void crawlSubfolders(final IFolder root, final List<IFile> files) throws CoreException {
-		if(!root.exists())
+		if (!root.exists())
 			return;
 		IResource[] members = root.members();
-		for(int i=0; i<members.length; i++) {
+		for (int i = 0; i < members.length; i++) {
 			IResource current = members[i];
-			if(current.getType() == IResource.FILE) {
+			if (current.getType() == IResource.FILE) {
 				files.add((IFile) current);
-			}else if (current.getType() == IResource.FOLDER) {
+			} else if (current.getType() == IResource.FOLDER) {
 				crawlSubfolders((IFolder) current, files);
 			}
 		}
-		
+
 	}
-	
-	public static void addComponentsToDefinition(SimDefinition definition, GTRuleSet rules, IBeXPatternSet patterns, IntermediateModelContainer intermediateModel) {
-		
-		intermediateModel.getComponents().stream()
-			.filter(component -> (component instanceof IntermRule))
-			.map(rule -> (IntermRule)rule).forEach(rule -> {
-				addRuleRateAnnotation(definition, rules, rule.getName(), rule.getRate());
-			});
-		
-		intermediateModel.getComponents().stream()
-			.filter(component -> (component instanceof IntermObservable))
-			.map(obs -> (IntermObservable)obs).forEach(obs -> {
-				addPatternObservation(definition, "obs_"+obs.getName(), patterns);
-			});
-		
-		intermediateModel.getComponents().stream()
-			.filter(component -> (component instanceof IntermCommand))
-			.map(comm -> (IntermCommand)comm).forEach(comm -> {
-				switch (comm.getType()) {
-				case TIME:
-					addTerminationConditionTime(definition, comm.getCnt());
-					break;
-				case ITERATIONS:
-					addTerminationConditionIterations(definition, (int)comm.getCnt());
-					break;
-				case MATCHES:
-					addTerminationConditionPattern(definition, comm.getPatternToMatch().getName(), (int)comm.getCnt());	//TODO: deliver correct pattern name parameter
-				}
-			});
-		
+
+	public static void addComponentsToDefinition(SimDefinition definition, GTRuleSet rules, IBeXPatternSet patterns,
+			IntermediateModelContainer intermediateModel) {
+
+		intermediateModel.getComponents().stream().filter(component -> (component instanceof IntermRule))
+				.map(rule -> (IntermRule) rule).forEach(rule -> {
+					addRuleRateAnnotation(definition, rules, rule.getName(), rule.getRate());
+				});
+
+		intermediateModel.getComponents().stream().filter(component -> (component instanceof IntermObservable))
+				.map(obs -> (IntermObservable) obs).forEach(obs -> {
+					addPatternObservation(definition, "obs_" + obs.getName(), patterns);
+				});
+
+		intermediateModel.getComponents().stream().filter(component -> (component instanceof IntermCommand))
+				.map(comm -> (IntermCommand) comm).forEach(comm -> {
+					switch (comm.getType()) {
+					case TIME:
+						addTerminationConditionTime(definition, comm.getCnt());
+						break;
+					case ITERATIONS:
+						addTerminationConditionIterations(definition, (int) comm.getCnt());
+						break;
+					case MATCHES:
+						addTerminationConditionPattern(definition, comm.getPatternToMatch().getName(),
+								(int) comm.getCnt()); // TODO: deliver correct pattern name parameter
+					}
+				});
+
 	}
-	
+
 	public static void addRuleRateAnnotation(SimDefinition definition, GTRuleSet rules, String ruleName, double rate) {
 		GTRule rule = null;
 		for (GTRule r : rules.getRules()) {
@@ -265,7 +275,7 @@ public class ReactionModelBuilder implements ModelBuilderExtension {
 		annotation.setRate(rate);
 		definition.getRuleAnnotations().add(annotation);
 	}
-	
+
 	public static void addPatternObservation(SimDefinition definition, String patternName, IBeXPatternSet patterns) {
 		IBeXContextPattern pattern = null;
 		for (IBeXContext p : patterns.getContextPatterns()) {
@@ -283,7 +293,7 @@ public class ReactionModelBuilder implements ModelBuilderExtension {
 		obs.setName(patternName);
 		definition.getObservations().add(obs);
 	}
-	
+
 	public static void addTerminationConditionIterations(SimDefinition definition, int iterations) {
 		SimpleTerminationCondition term = SimulationDefinitionFactory.eINSTANCE.createSimpleTerminationCondition();
 		term.setMaxIterations(iterations);
